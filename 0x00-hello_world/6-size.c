c9711b5caf4f259d55f0b99d0e859fe599a8d450
root <root@DESKTOP-SEJ1LM5>
2023-06-15 20:57:03 +0300

    lulu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main (void)
 	long long int d;
 	float z; 
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long ibt: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a long long ibt: %lu byte(s)\n",(unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(z));
 return (0);
 }
